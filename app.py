3f1a73c48f8130aed6e1db3a37ba6e8ba873e0c0
Nathan elliott <elliottnathan800@gmail.com>
2025-05-30 19:47:34 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,54 +1,67 @@
-
-from flask import Flask, render_template, request, jsonify, send_file
-from flask_cors import CORS
+from flask import Flask, request, jsonify, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import os
 import datetime
 
 app = Flask(__name__)
 CORS(app)
-
 app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['JWT_SECRET_KEY'] = 'your-jwt-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    joined = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    subscription_active = db.Column(db.Boolean, default=False)
+    trial_start = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    subscribed = db.Column(db.Boolean, default=False)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
-@app.route('/register', methods=['POST'])
-def register():
-    data = request.get_json()
-    user = User(username=data['username'], password=data['password'])
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    if User.query.filter_by(email=data['email']).first():
+        return jsonify({"msg": "User already exists"}), 409
+    user = User(email=data['email'], password=data['password'])
     db.session.add(user)
     db.session.commit()
-    return jsonify(message="User registered"), 201
+    token = create_access_token(identity=user.id)
+    return jsonify({"token": token})
 
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    user = User.query.filter_by(username=data['username'], password=data['password']).first()
-    if user:
-        access_token = create_access_token(identity=user.username)
-        return jsonify(access_token=access_token), 200
-    return jsonify(message="Invalid credentials"), 401
+    data = request.json
+    user = User.query.filter_by(email=data['email'], password=data['password']).first()
+    if not user:
+        return jsonify({"msg": "Invalid credentials"}), 401
+    token = create_access_token(identity=user.id)
+    return jsonify({"token": token})
 
-@app.route('/generate-video', methods=['POST'])
+@app.route('/subscription/activate', methods=['POST'])
 @jwt_required()
-def generate_video():
-    user = get_jwt_identity()
-    return jsonify(message="Video generation endpoint called by user: " + user)
+def activate_subscription():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    user.subscribed = True
+    db.session.commit()
+    return jsonify({"msg": "Subscription activated"})
+
+@app.route('/status', methods=['GET'])
+@jwt_required()
+def status():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    trial_end = user.trial_start + datetime.timedelta(days=3)
+    trial_active = datetime.datetime.utcnow() < trial_end
+    return jsonify({"trial": trial_active, "subscribed": user.subscribed})
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
